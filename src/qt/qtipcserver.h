c1fc4c2d8b4fdca68e66217bc0e688143c0770f5
x314 <hello@x314.network>
2017-12-19 19:23:29 +0100

    master

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define X314-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "X314URI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
